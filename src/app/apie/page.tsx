d47699c1457783314babe92a4487b651cf0cefc4
estaniulyte <43463977+estaniulyte@users.noreply.github.com>
2023-12-06 22:27:26 +0200

    add about page

---
+++ src/app/apie/page.tsx
@@ -0,0 +1,18 @@
+import Header from '@/components/Header';
+import Procedures from '@/components/Procedures';
+
+import Image from 'next/image';
+
+import { procedures } from '@/data';
+import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import Intro from '@/components/Intro';
+
+export default function Home() {
+  return (
+    <main className='pt-12'>
+      <MaxWidthWrapper>
+        <p className='pt-9'>apie</p>
+      </MaxWidthWrapper>
+    </main>
+  );
+}
